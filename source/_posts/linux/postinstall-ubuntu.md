4165874a2a14ec26f4b6f731ab130f1f066a0ef2
Wen-Wei Tseng <sosiristseng@gmail.com>
2020-11-20 14:42:37 +0800

    Ubuntu post install
    Fix #7

--- source/_posts/linux/postinstall-ubuntu.md
+++ source/_posts/linux/postinstall-ubuntu.md
@@ -18,16 +18,45 @@ Install the locale and then set locale to `zh_TW` solve the problem.
 
 ## Post install script
 
+```bash
+wget -q https://sosiristseng.github.io/code/postinstall/ubuntu/postinstall.bash
+
+# Check before running the script
+bash postinstall.bash
+```
+
 {% include_code postinstall/ubuntu/postinstall.bash %}
 
 Installed apps:
 
 {% include_code postinstall/ubuntu/pkgs.txt %}
 
+## Nvidia GPU
 
-## Download binaries
+Nvidia GPU driver and CUDA 11 runtime.[^cuda]
 
-(If needed)
+{% include_code postinstall/ubuntu/cuda.bash %}
+
+[^cuda]: [CUDA Toolkit 11.1 Update 1 Downloads](https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&target_distro=Ubuntu&target_version=2004&target_type=debnetwork)
+
+## AMD GPU setup
+
+AMD Mesa Driver Install[^ChrisTitus]
+
+{% include_code postinstall/ubuntu/amd-mesa.bash %}
+
+[^ChrisTitus]: [The Ultimate Linux Gaming Guide](https://christitus.com/ultimate-linux-gaming-guide/) by Chris Titus
+
+## Extensions for gnome shell
+
+From the [gnome shell website](https://extensions.gnome.org/) + browser addon
+- [User themes](https://extensions.gnome.org/extension/19/user-themes/)
+- [Sound Input & Output Device Chooser](https://extensions.gnome.org/extension/906/sound-output-device-chooser/)
+- [Screenshot Tool](https://extensions.gnome.org/extension/1112/screenshot-tool/)
+- [Dash to panel](https://extensions.gnome.org/extension/1160/dash-to-panel/) for a Win-8sque experience
+- [Material shell](https://extensions.gnome.org/extension/3357/material-shell/) for tiling windows experience
+
+## Download binaries if needed
 
 - [Telegram](https://telegram.org/)
 - [MarkText (AppImage)](https://github.com/marktext/marktext)
--- source/code/postinstall/ubuntu/postinstall.bash
+++ source/code/postinstall/ubuntu/postinstall.bash
@@ -11,7 +11,9 @@ sudo sed -i 's/us.archive.ubuntu.com/free.nchc.org.tw/g' /etc/apt/sources.list
 sudo sed -i 's/tw.archive.ubuntu.com/free.nchc.org.tw/g' /etc/apt/sources.list
 sudo sed -i 's/security.ubuntu.com/free.nchc.org.tw/g' /etc/apt/sources.list
 sudo apt update && sudo apt install -y apt-transport-https ca-certificates curl git gnupg-agent software-properties-common python3-pip
-sudo dpkg --add-architecture i386
+
+# Run this line if you need Wind and games
+# sudo dpkg --add-architecture i386
 
 # Google chrome
 wget -qO- https://dl.google.com/linux/linux_signing_key.pub | sudo tee /etc/apt/trusted.gpg.d/google-chrome.asc > /dev/null
@@ -20,6 +22,10 @@ echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sud
 # Vivaldi
 wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | sudo tee /etc/apt/trusted.gpg.d/vivaldi.asc > /dev/null
 echo "deb https://repo.vivaldi.com/archive/deb/ stable main" | sudo tee /etc/apt/sources.list.d/vivaldi.list > /dev/null
+
+# Brave
+wget -qO- https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo tee /etc/apt/trusted.gpg.d/brave-browser-release.asc > /dev/null
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list > /dev/null
 
 # Anydesk
 wget -qO- https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo tee /etc/apt/trusted.gpg.d/anydesk.asc > /dev/null
@@ -32,20 +38,20 @@ echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -c
 # Zotero
 wget -qO- https://github.com/retorquere/zotero-deb/releases/download/apt-get/install.sh | sudo bash > /dev/null
 
+# Typora
+wget -qO - https://typora.io/linux/public-key.asc | sudo tee /etc/apt/trusted.gpg.d/typora.asc > /dev/null
+echo 'deb https://typora.io/linux ./' | sudo tee /etc/apt/sources.list.d/typora.list > /dev/null
+
 # Xanmod linux kernel
 wget -qO- https://dl.xanmod.org/gpg.key | sudo tee /etc/apt/trusted.gpg.d/xanmod.asc > /dev/null
 echo 'deb http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-kernel.list > /dev/null
-
-# Brave
-wget -qO- https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo tee /etc/apt/trusted.gpg.d/brave-browser-release.asc > /dev/null
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list > /dev/null
 
 sudo add-apt-repository -yu ppa:appimagelauncher-team/stable  # AppImageLauncher
 sudo add-apt-repository -yu ppa:git-core/ppa              # Git stable releases
 sudo add-apt-repository -yu ppa:alessandro-strada/ppa     # Google drive client
 sudo add-apt-repository -yu ppa:papirus/papirus           # Papirus icon theme
 sudo add-apt-repository -yu ppa:yann1ck/onedrive          # OneDrive client
-sudo add-apt-repository -yu ppa:graphics-drivers/ppa      # Nvidia Proprietary Driver
+# sudo add-apt-repository -yu ppa:graphics-drivers/ppa      # Nvidia Proprietary Driver
 sudo add-apt-repository -yu ppa:atareao/telegram          # Telegram
 
 # Set up apt-fast
@@ -61,11 +67,11 @@ sudo apt-fast full-upgrade -y
 # Downoad and install deb
 wget -qO /tmp/teamviewer.deb "https://download.teamviewer.com/download/linux/teamviewer_amd64.deb"
 
-sudo apt install -y /tmp/*.deb
+sudo apt-fast install -y /tmp/*.deb
 
 # Snap packages
 sudo snap install gitkraken --classic
-sudo snap install telegram
+sudo snap install code --classic
 
 wget -qO- https://sosiristseng.github.io/code/postinstall/ubuntu/ubuntu.txt | sed 's/#.*$//' | xargs sudo apt-fast install -y
 
